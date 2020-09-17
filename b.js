888956c2f5de53890d45a3c98435b007a810d6f9
keyunlei <keyunlei@corp.netease.com>
2020-09-17 15:23:06 +0800

    feat: postMessage简单通信

---
+++ b.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const fs = require('fs');
+const { resolve } = require('path');
+
+const server = http.createServer(function(req, res) {
+    res.writeHeader(200, { 'Content-Type': 'text/html;charset=utf-8'});
+    let data = fs.readFileSync(resolve(__dirname, './b.html'));
+    res.end(data);
+});
+
+server.listen(8001, '127.0.0.1');
+console.log('http://127.0.0.1:8001');
